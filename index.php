6dd9a1382daa1d655e36e44f2342cc4050cbdb70
douglas-srs <douglas.srs12@gmail.com>
2015-11-17 15:58:37 -0200

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ header('Content-Type: text/html; charset=UTF-8');
 include('correios.class.php');
 if(isset($_GET['cep'])){
 	die(json_encode(Correios::cep($_GET['cep'])));
-}elseif(isset($_GET['codigo-rastreio'])){
-	die(json_encode(Correios::rastreio($_GET['codigo-rastreio'])));
+}elseif(isset($_GET['codigo_rastreio'])){
+	die(json_encode(Correios::rastreio($_GET['codigo_rastreio'])));
 }else{
-	die('informe parametro GET cep ou codigo-rastreio');
+	die('informe parametro GET <strong>cep</strong> ou <strong>codigo_rastreio</strong>');
 }
