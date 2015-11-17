70bdc22ee5d51f79bbfcd7727625139026a22ef7
douglas-srs <douglas.srs12@gmail.com>
2015-11-17 16:11:38 -0200

    Update correios.class.php

--- correios.class.php
+++ correios.class.php
@@ -131,6 +131,7 @@ class Correios {
 	}
 	
 	static public function rastreio($codigo){
+		include('phpQuery-onefile.php');
 		$html = self::simpleCurl('http://websro.correios.com.br/sro_bin/txect01$.QueryList?P_LINGUA=001&P_TIPO=001&P_COD_UNI='.$codigo);
 		phpQuery::newDocumentHTML($html, $charset = 'utf-8');
 
